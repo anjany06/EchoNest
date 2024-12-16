d4b93954aa8d51c7d0ff623e974a441fff139e68
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-17 01:48:57 +0530

    `Replaced mobile device redirect with alert message`

--- script.js
+++ script.js
@@ -8,12 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Redirect or show a message if on a mobile device
   if (isMobileDevice()) {
-      // Option 1: Redirect to a different page
-      window.location.href = "unsupported.html"; // Create an unsupported.html page
-
-      // Option 2: Show an alert message (uncomment the next line if you prefer this option)
-      // alert("This website is not supported on mobile devices. Please visit on a desktop.");
-  }
+  alert("This website is not supported on mobile devices. Please visit on a desktop.");
+   }
   
   // Existing code for responsive navbar
   const navbar = document.querySelector('.navbar');
